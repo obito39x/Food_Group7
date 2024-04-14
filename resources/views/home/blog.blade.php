fa75a37754a3660e6e552f42583e17758a54c48f
nguyenanhtuan <104258893+obito39x@users.noreply.github.com>
2024-04-14 14:32:57 +0700

    Fix broken interface due to menu

--- resources/views/home/blog.blade.php
+++ resources/views/home/blog.blade.php
@@ -8,6 +8,8 @@
     <link href="{{ asset('/css/blog.css') }}" rel="stylesheet">
     <link rel="short icon" href="image/short_icon.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="{{ asset('/js/menu.js') }}"></script>
 </head>
 @extends('layouts.app') <!-- Extend the main layout -->
 

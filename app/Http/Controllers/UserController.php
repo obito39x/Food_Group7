510e0c22dcd11732a1e06546206be1c2608f246f
nguyenanhtuan <104258893+obito39x@users.noreply.github.com>
2024-05-10 14:32:40 +0700

    function  edit profile, fix some bug

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,13 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function profile(){
-    $account = Auth::user();
-    $user = $account->user;
-    return view('profile.profile', ['profile' => $user]);
-}
+        $account = Auth::user();
+        $user = $account->user;
+        return view('profile.profile', ['profile' => $user]);
+    }
+
+    public function updateProfile(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $request->validate([
+            'email' => 'required|email|unique:users,email,' . $user->id . '|unique:accounts,email,' . $user->account->id,
+        ]);
+        $data = $request->only(['username', 'fullname', 'email', 'phone_number', 'gender', 'date_user', 'img']);
+        
+
+        if ($request->hasFile('image')) {
+            $file = $request->file('image');
+            $filename = time() . '.' . $file->getClientOriginalExtension();
+            $file->move(public_path('img_profile'), $filename);
+            $data['img'] = '/img_profile/' . $filename;
+        }
+
+        $user->update($data);
+        return redirect()->back();
+    }
 
 }
+ 
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,14 +7,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
+    
+    protected $table = 'users';
     public $timestamps = false;
     protected $fillable = ['id_account', 'username', 'fullname', 'email', 'phone_number', 'gender', 'date_user', 'img'];
-
+    
     // public function account(){
-    //     return $this->hasOne(Account::class, 'id_account', 'id');
-    // }
+        //     return $this->hasOne(Account::class, 'id_account', 'id');
+        // }
+        protected static function boot()
+        {
+            parent::boot();
+    
+            static::updated(function ($user) {
+                if ($user->isDirty('email')) {  // Kiểm tra xem 'email' có được thay đổi hay không
+                    $user->account()->update([
+                        'email' => $user->email
+                    ]);
+                }
+            });
+        }
     public function account(){
-    return $this->belongsTo(Account::class, 'id_account', 'id');
+    return $this->belongsTo(Account::class, 'id_account');
+
+    
 }
 
 }
+ 
